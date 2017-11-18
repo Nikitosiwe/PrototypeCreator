7cf7a834f12b12b1f9ba39a111e64697ec7a8ddf
Nikitosiwe <nikitos673@gmail.com>
2017-11-18 20:44:40 +0200

    Добавлены слоты и сигналы для работы с моделью и общения с представлением.

--- Programm/Main.py
+++ Programm/Main.py
@@ -5,10 +5,32 @@ from PyQt5.QtWidgets import QApplication
 # Класс QQuickView предоставляет возможность отображать QML файлы.
 from PyQt5.QtQuick import QQuickView
 
+import PageModel as PM
+
 
 class Context(QObject):
     def __init__(self):
         QObject.__init__(self)
+        self.pageList=[]
+
+    createPageSignal = pyqtSignal(int,int,int, arguments=['ID','X','Y'])
+    addElementToPageSignal= pyqtSignal(str,int,int,int, arguments=['ElementName','X','Y', 'pageID'])
+
+    @pyqtSlot(int,int,int,int,str)
+    def createPage(self, x, y, width, height, color):
+        print("CreatePage", x, y, width, height, color)
+        id = 1
+        if len(self.pageList) > 0:
+            id =  self.pageList[-1].id+1
+
+        self.pageList.append(PM.PageModel(id, x, y, width, height, color))
+        self.createPageSignal.emit(id, x, y)
+
+    @pyqtSlot(str,int,int,int,int,int,str)
+    def addElementToPage(self, elementName, pageId, x, y, width, height, color):
+        print("AddElement", self.pageList)
+        self.pageList[pageId-1].addElement(elementName, x, y, width, height, color)
+        self.addElementToPageSignal.emit(elementName, x, y, pageId)
 
     @pyqtSlot(str)
     def printConsoleMessage(self, text):
@@ -16,6 +38,12 @@ class Context(QObject):
 
     @pyqtSlot()
     def showPreview(self):
+        for p in self.pageList:
+            print("Page:",p.id)
+            for e in p.elementList:
+                print("element",e.id)
+
+
         view1 = QQuickView()
         view1.setSource(QUrl('QML/PreviewWindow.qml'))
 
