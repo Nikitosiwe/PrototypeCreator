f88aebb7f0042eb961acd57021a7194c44c9109a
Nikitosiwe <nikitos673@gmail.com>
2017-11-19 14:09:31 +0200

    ГЛавное окно выделенно в отдельный файл. Класс контекста данных главного окна перенесен в отдельный файл.

--- Programm/Main.py
+++ Programm/Main.py
@@ -5,58 +5,29 @@ from PyQt5.QtWidgets import QApplication
 # Класс QQuickView предоставляет возможность отображать QML файлы.
 from PyQt5.QtQuick import QQuickView
 
-import PageModel as PM
+import ContextPreview as CP
+import MainContext as MC
 
 
-class Context(QObject):
-    def __init__(self):
-        QObject.__init__(self)
-        self.pageList=[]
+class MainWindow(QQuickView):
+    def __init__(self, parent=None):
+        QQuickView.__init__(self, parent)
 
-    createPageSignal = pyqtSignal(int,int,int, arguments=['ID','X','Y'])
-    addElementToPageSignal= pyqtSignal(str,int,int,int, arguments=['ElementName','X','Y', 'pageID'])
-
-    @pyqtSlot(int,int,int,int,str)
-    def createPage(self, x, y, width, height, color):
-        print("CreatePage", x, y, width, height, color)
-        id = 1
-        if len(self.pageList) > 0:
-            id =  self.pageList[-1].id+1
-
-        self.pageList.append(PM.PageModel(id, x, y, width, height, color))
-        self.createPageSignal.emit(id, x, y)
-
-    @pyqtSlot(str,int,int,int,int,int,str)
-    def addElementToPage(self, elementName, pageId, x, y, width, height, color):
-        print("AddElement", self.pageList)
-        self.pageList[pageId-1].addElement(elementName, x, y, width, height, color)
-        self.addElementToPageSignal.emit(elementName, x, y, pageId)
-
-    @pyqtSlot(str)
-    def printConsoleMessage(self, text):
-        print(text)
-
-    @pyqtSlot()
-    def showPreview(self):
-        for p in self.pageList:
-            print("Page:",p.id)
-            for e in p.elementList:
-                print("element",e.id)
-
-
-        view1 = QQuickView()
-        view1.setSource(QUrl('QML/PreviewWindow.qml'))
+        self.setSource(QUrl('QML/MainWindow.qml'))
+        self.mainContext = MC.Context(self)
+        self.rootContext().setContextProperty("mainContext", self.mainContext)
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     # Объект QQuickView, в который грузится UI для отображения
-    view = QQuickView()
-    view.setSource(QUrl('QML/MainWindow.qml'))
+   # view = QQuickView()
+   # view.setSource(QUrl('QML/MainWindow.qml'))
 
-    mainContext = Context()
-    view.rootContext().setContextProperty("mainContext", mainContext)
+   # mainContext = Context()
+   # view.rootContext().setContextProperty("mainContext", mainContext)
 
-    #view.show()
-    app.exec_()
-    sys.exit()
+    mainWindow = MainWindow()
+
+    #mainWindow.show()
+    sys.exit(app.exec_())
