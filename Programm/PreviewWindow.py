e651334a7930d07d59f4b3049ac8cb73c5e1980d
Nikitosiwe <nikitos673@gmail.com>
2017-11-19 14:07:24 +0200

    Создан класс окна предпросмотра

---
+++ Programm/PreviewWindow.py
@@ -0,0 +1,15 @@
+import sys
+
+from PyQt5.QtCore import QUrl, QObject, pyqtSignal, pyqtSlot
+
+from PyQt5.QtQuick import QQuickView
+
+import ContextPreview as CP
+
+class PreviewWindow(QQuickView):
+    def __init__(self, parent=None):
+        QQuickView.__init__(self, parent)
+
+        self.setSource(QUrl('QML/PreviewWindow.qml'))
+        self.previewContext = CP.ContextPreview()
+        self.rootContext().setContextProperty("previewContext", self.previewContext)
