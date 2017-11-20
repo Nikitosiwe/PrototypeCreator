6850819d7cae323be688fec60c64f72e8dd88a85
Nikitosiwe <nikitos673@gmail.com>
2017-11-20 15:26:40 +0200

    Добавил слот отистки модели

--- Programm/MainContext.py
+++ Programm/MainContext.py
@@ -11,28 +11,38 @@ class Context(QObject):
         self.pageList=[]
         self.parent = parent
 
+
     createPageSignal = pyqtSignal(int,int,int, arguments=['ID','X','Y'])
     addElementToPageSignal= pyqtSignal(str,int,int,int, arguments=['ElementName','X','Y', 'pageID'])
 
-    @pyqtSlot(int,int,int,int,str)
-    def createPage(self, x, y, width, height, color):
+
+
+    @pyqtSlot(int,int,int,int,int,str)
+    def createPage(self, isPreview, x, y, width, height, color):
         print("CreatePage", x, y, width, height, color)
         id = 1
         if len(self.pageList) > 0:
             id =  self.pageList[-1].id+1
 
         self.pageList.append(PM.PageModel(id, x, y, width, height, color))
-        self.createPageSignal.emit(id, x, y)
+        if isPreview==0:
+            self.createPageSignal.emit(id, x, y)
 
-    @pyqtSlot(str,int,int,int,int,int,str)
-    def addElementToPage(self, elementName, pageId, x, y, width, height, color):
-        print("AddElement", self.pageList)
-        self.pageList[pageId-1].addElement(elementName, x, y, width, height, color)
-        self.addElementToPageSignal.emit(elementName, x, y, pageId)
+    @pyqtSlot(int,str,int,int,int,int,int,str)
+    def addElementToPage(self,isPreview, elementName, pageId, x, y, width, height, color):
+        #print("AddElement", self.pageList)
+        self.pageList[pageId-1].addElement(elementName, x, y, width, height , color)
+        if isPreview == 0:
+            self.addElementToPageSignal.emit(elementName, x, y, pageId)
+
 
     @pyqtSlot(str)
     def printConsoleMessage(self, text):
         print(text)
+
+    @pyqtSlot()
+    def clearModel(self):
+        self.pageList.clear()
 
     @pyqtSlot()
     def showPreview(self):
@@ -40,7 +50,7 @@ class Context(QObject):
         previewWindow = PW.PreviewWindow(self.parent)
 
         for p in self.pageList:
-            print("Page:",p.id)
+            print("Page:",p.id,p.color)
             for e in p.elementList:
                 print("element",e.id)
 
