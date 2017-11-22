b7b21fb364f850370d4c3ae4e4d1cd5b2e4dd3d8
Nikitosiwe <nikitos673@gmail.com>
2017-11-22 15:54:10 +0200

    Добавил LinkId в слот и сигнал добавления елемента

--- Programm/ContextPreview.py
+++ Programm/ContextPreview.py
@@ -6,12 +6,12 @@ class ContextPreview(QObject):
         self.parent = parent
 
 
-
     createPagePreviewSignal = pyqtSignal(int,int,int,str,arguments=['PageId','Width','Height','Color'])
 
-    createElementPreviewSignal = pyqtSignal(int, str, int, int, int, int, str, arguments=['PageID','ElementName', 'X', 'Y', 'Width', 'Height', 'Color'])
+    createElementPreviewSignal = pyqtSignal(int, str, int, int, int, int, str,int, arguments=['PageID','ElementName', 'X', 'Y', 'Width', 'Height', 'Color', 'LinkID'])
 
     changeLinkSignal = pyqtSignal(int, arguments=['LinkId'])
+
 
     @pyqtSlot(str)
     def printConsoleMessage(self, text):
@@ -26,6 +26,11 @@ class ContextPreview(QObject):
     @pyqtSlot()
     def initializeComponents(self):
         for PP in self.parent.parent.mainContext.pageList:
+            print("page",PP.id, PP.width, PP.height, PP.color)
             self.createPagePreviewSignal.emit(PP.id, PP.width, PP.height, PP.color)
             for EP in PP.elementList:
-                self.createElementPreviewSignal.emit(PP.id, EP.elementName, EP.x, EP.y, EP.width, EP.height, EP.color)
+                print("element", PP.id, EP.elementName, EP.x, EP.y, EP.width, EP.height, EP.color)
+                self.createElementPreviewSignal.emit(PP.id, EP.elementName,
+                                                     EP.x, EP.y, EP.width,
+                                                     EP.height, EP.color,
+                                                     EP.linkId)
